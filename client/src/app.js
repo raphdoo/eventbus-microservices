6489a093b0db21b03dd0835cad224f463a694fcd
Rapheal Ajiboye <raphoooo95@gmail.com>
2023-03-22 20:50:50 +0100

    setting up skaffold config file

--- client/src/app.js
+++ client/src/app.js
@@ -5,7 +5,7 @@ import PostList from './PostList'
 const App = () => {
   return (
     <div className='container'>
-        <h1>Create Post</h1>
+        <h1>Create Post title</h1>
         <PostCreate/>
         <hr/>
         <h1>Post</h1>
